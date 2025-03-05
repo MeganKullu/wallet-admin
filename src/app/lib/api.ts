81ec46d577e2d788779e73dc643c812181feb78f
MeganKullu <kullumegan@gmail.com>
2025-03-05 17:38:28 +0300

    added interceptor

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -20,6 +20,7 @@ api.interceptors.request.use((config) => {
             config.headers.Authorization = `Bearer ${token}`;
         }
     }
+    config.headers['Access-Control-Request-Method'] = 'GET, POST, PUT, DELETE, OPTIONS';
     return config;
 });
 
